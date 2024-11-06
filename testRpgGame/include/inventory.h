02f59c34bf1b9803bfd59ed3024300d651c38b2d
gayeongkim <gy.kim.g0@gmail.com>
2024-11-06 18:54:42 +0900

    흐름 정리 및 인벤토리 파일 생성

---
+++ testRpgGame/include/inventory.h
@@ -0,0 +1,19 @@
+#ifndef INVENTORY_H
+#define INVENTORY_H
+
+class Inventory {
+    public:
+        int health_item;
+        int attackPower_item;
+    
+    //item 성능 값 반환
+    void increase_item() {
+        //기준 사이에서 랜덤하게 높일 난수 생성, 생몀 공격력 각각 다른 난수
+        //매레밸 
+        //공력력 아이템 높여
+        //생명령 아이템 높여
+        //각 아이탬 성능 반환해
+    }
+}
+
+#endif
--- testRpgGame/include/player.h
+++ testRpgGame/include/player.h
@@ -2,6 +2,7 @@
 #define PLAYER_H
 
 #include <string>
+#include <vector>
 #include "character.h"
 
 class Player : public Character {
